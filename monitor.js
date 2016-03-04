8bd35fb66f0dc1b4b64af993aeadace6ca22cee0
pgarrison <philipgarrison@cmu.edu>
2016-03-04 12:18:57 +0800

    Remove clean_comments, fix bugs with exporting to csv

--- monitor.js
+++ monitor.js
@@ -4,24 +4,25 @@ var json2csv = require('json2csv');
 var async = require('async');
 
 var config = {
+  version: '2.5',
   post_limit: 25, 
   comment_limit: 500, // If this is too big, you may get errors stating "Please reduce the amount of data you're asking for, then retry your request"
   refresh_interval_ms: 1000*60, 
   backup_interval_ms: 1000*60*10,
   alive_age_ms: 1000*60*60*24,
   state_backup_file: './monitor.backup',
-  post_export_file: './monitor.export',
-  comments_export_file: './monitor.export',
+  post_export_file: './monitor_posts.export',
+  comment_export_file: './monitor_comments.export',
   access_token: 'APP_ID|APP_SECRET'
-}
-
-var state = {
   sources: [
     'some_fb_page',
     'some_fb_group',
     'not_a_user_though',
     'nytimes'
   ],
+}
+
+var state = {
   requests: [],
   live_posts: {},
   posts: {},
@@ -39,28 +40,32 @@ var comment_params = {
   filter: 'stream' // Get comment replies too
 };
 
-function format_post(post, source_id) {
-  return {
-    source_id: source_id,
-    id: post.id,
-    created_time: post.created_time,
-    updated_time: post.updated_time,
-    likes: post.likes.summary.total_count,
-    comments: post.comments.summary.total_count,
-    shares: post.shares ? post.shares.count
-                        : 0,
-    message: post.message
+function format_post(source_id) {
+  return function(post) {
+    return {
+      source_id: source_id,
+      id: post.id,
+      created_time: post.created_time,
+      updated_time: post.updated_time,
+      likes: post.likes.summary.total_count,
+      comments: post.comments.summary.total_count,
+      shares: post.shares ? post.shares.count
+                          : 0,
+      message: post.message
+    };
   };
 }
 
-function format_comment(comment, post_id) {
-  return {
-    post_id: post_id,
-    id: comment.id,
-    created_time: comment.created_time,
-    from_id: comment.from.id,
-    likes: comment.likes.summary.total_count,
-    message: comment.message
+function format_comment(post_id) {
+  return function(comment) {
+    return {
+      post_id: post_id,
+      id: comment.id,
+      created_time: comment.created_time,
+      from_id: comment.from.id,
+      likes: comment.likes.summary.total_count,
+      message: comment.message
+    };
   };
 }
 
@@ -120,19 +125,21 @@ function use_ids_as_keys(arr) {
   return _.object(_.zip(ids, arr));
 }
 
-function get_comments(post_id, after) {
+function get_comments(post_id, after, callback) {
   get('/' + post_id + '/comments', comment_params, after, function(res) {
     console.log('Got ' + res.data.length + ' comments');
+    var new_comments_arr = _.map(res.data, format_comment(post_id));
     state.comments = _.extend(state.comments, 
-                              use_ids_as_keys(_.map(res.data, format_comment)));
+                              use_ids_as_keys(new_comments_arr));
 
     if(res && res.paging && res.paging.cursors && res.paging.cursors.after) {
       var new_after = res.paging.cursors.after;
       if(after !== new_after) {
-        get_comments(post_id, new_after);
+        get_comments(post_id, new_after, callback);
       }
     } else {
       console.log('Got all comments.');
+      callback();
     }
   });
 }
@@ -142,7 +149,7 @@ function get_posts(source_id) {
   get('/' + source_id + '/feed', post_params, null, function(res) {
     console.log('Got ' + res.data.length + ' posts from ' + source_id);
 
-    var post_records = _.map(res.data, format_post);
+    var post_records = _.map(res.data, format_post(source_id));
 
     _.each(post_records, function(post) {
       var maybe_new_comments = !state.posts[post.id] || 
@@ -156,23 +163,23 @@ function get_posts(source_id) {
   });
 }
 
-function get_all_comments(post) {
+function get_all_comments(post, callback) {
   // By passing null here, the first request gets the oldest comments, and 
   // recursive requests get all of them.
-  get_comments(post.id, null); 
+  get_comments(post.id, null, callback); 
 }
 
 function prune_posts() {
   var alive_and_dying = _.partition(_.values(state.live_posts), 
                                     updated_recently);
   state.live_posts = use_ids_as_keys(alive_and_dying[0]);
-  _.each(alive_and_dying[1], get_all_comments);
+  async.each(alive_and_dying[1], get_all_comments);
   console.log('Pruned ' + alive_and_dying[1].length + ' posts. ' + 
               alive_and_dying[0].length + ' posts remain alive.');
 }
 
 function check_all_sources() {
-  _.each(state.sources, get_posts);
+  _.each(config.sources, get_posts);
 }
 
 var save = fs.writeFileSync;
@@ -192,39 +199,36 @@ function load_state(filename) {
   state = JSON.parse(fs.readFileSync(filename));
 }
 
-// Caution: this will create duplicate comments when run twice or when these
-// posts "die."
-function save_live_post_comments() {
-  _.each(state.live_posts, get_all_comments);
+function save_live_post_comments(callback) {
+  async.each(state.live_posts, get_all_comments, callback);
 }
 
-// Remove duplicate comments in case of injudicious use of 
-// save_live_post_comments
-function clean_comments() {
-  unique_comments = _.uniq(state.comments, function(comment) {
-    return comment.id;
+function save_csv(filename, obj) {
+  var arr = _.values(obj);
+  json2csv({ data: arr, fields: _.keys(arr[0]) }, function(err, csv) {
+    if(err) {
+      console.log(err);
+      throw new Error(err.message);
+    }
+    save(filename, csv);
   });
-  console.log('Found ' + (state.comments.length - unique_comments.length) + ' duplicate comments.');
-  state.comments = unique_comments;
-}
-
-function to_csv(arr) {
-  if(!arr || typeof arr.length !== 'number' || arr.length <= 0) {
-    return '';
-  }
-  return json2csv({ data: arr, fields: _.keys(arr[0]) });
 }
 
 // Export all data as two csv files, including comments from live posts
 function export_all() {
-  save_live_post_comments();
-  clean_comments();
-  save(to_csv(state.posts), config.post_export_file);
-  save(to_csv(state.comments), comment_export_file);
+  save_live_post_comments(function(err) {
+    if(err) {
+      console.log(err);
+      throw new Error(err.message);
+    }
+    save_csv(config.post_export_file, state.posts);
+    save_csv(config.comment_export_file, state.comments);
+    console.log('Export complete');
+  });
 }
 
 function initialize() {
-  graph.setVersion('2.5');
+  graph.setVersion(config.version);
   graph.setAccessToken(config.access_token);
 }
 
@@ -240,7 +244,7 @@ function start() {
 // before this.
 function test_sources() {
   var posts = 0;
-  async.each(state.sources, function(source_id, callback) {
+  async.each(config.sources, function(source_id, callback) {
     get('/' + source_id + '/feed', post_params, null, function(res) {
       if(res.data.length === 0) {
         callback(new Error('Source ' + source_id + ' returned 0 posts'));
@@ -253,9 +257,9 @@ function test_sources() {
     if(err) {
       console.log(err);
     } else {
-      console.log('Successfully got posts (' + posts + ') from all sources (' + state.sources.length + ').');
-      if(state.sources.length * config.post_limit !== posts) {
-        console.log('Warning: hoped to get ' + (state.sources.length * config.post_limit) + ' posts, but only got ' + posts + '.');
+      console.log('Successfully got posts (' + posts + ') from all sources (' + config.sources.length + ').');
+      if(config.sources.length * config.post_limit !== posts) {
+        console.log('Warning: hoped to get ' + (config.sources.length * config.post_limit) + ' posts, but only got ' + posts + '.');
       }
     }
   });
@@ -268,6 +272,10 @@ function getlots(n) {
   });
 }
 
+function get_state() {
+  return state;
+}
+
 module.exports = {
   start: start,
   export_all: export_all,
@@ -276,9 +284,10 @@ module.exports = {
     getlots: getlots,
     test_sources: test_sources,
     save_live_post_comments: save_live_post_comments,
-    clean_comments: clean_comments,
     export_state: export_state,
     load_state: load_state,
-    state: state
+    get_state: get_state,
+    save_csv: save_csv,
+    config: config
   }
 };
